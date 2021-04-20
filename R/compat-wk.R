3ac23aa86408b7d235234c7f98c89a2ae25d39df
Dewey Dunnington <dewey@fishandwhistle.net>
2021-04-19 22:08:31 -0300

    skeleton wk methods

--- R/compat-wk.R
+++ R/compat-wk.R
@@ -19,3 +19,67 @@ h3_index_point_writer <- function(res) {
 
   wk::new_wk_handler(.Call(h3r_c_point_writer_new, res), "h3_index_point_handler")
 }
+
+#' @rdname h3_index_point_writer
+#' @export
+h3_set_writer <- function(res) {
+  stop("Not implemented")
+}
+
+#' Export H3 Index/Set objects to geometry
+#'
+#' @param x An [h3_index()] or [h3_set()]
+#' @param what One of "center", "boundary", or "polygon".
+#'   For [h3_index()] methods, this will return a point,
+#'   linestring, or polygon feature. For [h3_set()] methods,
+#'   this will return a multipoint (centre of cells in the set),
+#'   a multilinestring (cumulative boundary of cells not including
+#'   internal boundaries), or multipolygon (cumulative area of cells
+#'   not including internal boundaries).
+#' @param ... Unused
+#'
+#' @rdname h3-export
+#' @importFrom wk as_xy
+#' @export
+as_xy.h3_index <- function(x, ...) {
+  stop("Not implemented")
+}
+
+#' @rdname h3-export
+#' @importFrom wk as_wkb
+#' @export
+as_wkb.h3_index <- function(x, what, ...) {
+  what <- match.arg(what, c("center", "boundary", "polygon"))
+  stop("Not implemented")
+}
+
+#' @rdname h3-export
+#' @importFrom wk as_wkt
+#' @export
+as_wkt.h3_index <- function(x, what, ...) {
+  what <- match.arg(what, c("center", "boundary", "polygon"))
+  stop("Not implemented")
+}
+
+#' @rdname h3-export
+#' @importFrom wk as_xy
+#' @export
+as_xy.h3_set <- function(x, ...) {
+  stop("Not implemented")
+}
+
+#' @rdname h3-export
+#' @importFrom wk as_wkb
+#' @export
+as_wkb.h3_set <- function(x, what, ...) {
+  what <- match.arg(what, c("center", "boundary", "polygon"))
+  stop("Not implemented")
+}
+
+#' @rdname h3-export
+#' @importFrom wk as_wkt
+#' @export
+as_wkt.h3_set <- function(x, what, ...) {
+  what <- match.arg(what, c("center", "boundary", "polygon"))
+  stop("Not implemented")
+}
--- R/h3-set.R
+++ R/h3-set.R
@@ -1,5 +1,14 @@
 
+#' Create H3 Set vectors
+#'
+#' @param h An [h3_index()] vector.
+#' @param group_id A vector defining the groups into which
+#'  `h` should be split. Changes in sequential values define
+#'  groups.
+#'
+#' @return A vctr of class "h3_set"
+#' @export
+#'
 h3_set <- function(h, group_id = 1L) {
-
+  stop("Not implemented")
 }
-
