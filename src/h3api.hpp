db2971ff18f68fcbcad2de1c03bc7541be32a8c3
Anthony North <anthony.jl.north@gmail.com>
2023-07-22 19:31:30 +1000

    use na_real for null

--- src/h3api.hpp
+++ src/h3api.hpp
@@ -1,17 +1,25 @@
 #pragma once
 
+#define R_NO_REMAP
+#include <R.h>
 #include <array>
 #include <charconv>
 #include <stdexcept>
 #include <string>
 #include <system_error>
-
 #include "h3/h3api.h"
+#include "utils.hpp"
+
+const H3Index h3_null = bit_cast<H3Index>(NA_REAL);
+
+bool h3_is_null(H3Index h3_index) {
+  return h3_index == h3_null;
+}
 
 H3Index h3_from_str(std::string_view str) {
-  H3Index h3_index = H3_NULL;
-  if (str.empty()) return h3_index;
+  if (str.empty()) return h3_null;
 
+  H3Index h3_index = h3_null;
   auto [ptr, ec] = std::from_chars(str.begin(), str.end(), h3_index, 16);
 
   if (ec != std::errc() || *ptr != '\0')
@@ -21,7 +29,7 @@ H3Index h3_from_str(std::string_view str) {
 }
 
 std::string_view h3_to_str(H3Index h3_index, std::array<char, 17>& str) {
-  if (h3_index == H3_NULL) return std::string_view();
+  if (h3_is_null(h3_index)) return std::string_view();
 
   auto [ptr, ec] = std::to_chars(str.begin(), str.end(), h3_index, 16);
 
