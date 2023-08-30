4fed0ddad976b1f70bb5cd05898b6882f7ab3d61
Anthony North <anthony.jl.north@gmail.com>
2023-08-31 08:59:41 +1000

    cleanup cell writer

--- src/wk-writer.cpp
+++ src/wk-writer.cpp
@@ -3,56 +3,69 @@
 #include <Rinternals.h>
 #include "h3api.hpp"
 #include "r-vector.hpp"
-#include "wk.hpp"
 #include "vctrs.hpp"
+#include "wk.hpp"
 
 struct CellWriter : wk::Handler {
   using Result = wk::Result;
 
   CellWriter(int res) : res_(res) {}
 
-  virtual Result vector_start(const wk_vector_meta_t* meta) override {
+  Result vector_start(const wk_vector_meta_t* meta) override {
     if (meta->size != WK_VECTOR_SIZE_UNKNOWN) result_.reserve(meta->size);
     return Result::Continue;
   }
 
-  virtual Result null_feature() override {
-    result_.push_back(h3_null);
+  Result feature_start(const wk_vector_meta_t* meta) override {
+    ++feat_id_;
+    coord_id_ = -1;
     return Result::Continue;
   }
 
-  virtual Result geometry_start(const wk_meta_t* meta) override {
-    using namespace std::string_literals;
+  Result geometry_start(const wk_meta_t* meta) override {
     // EMPTY and any set of features that (could) contain a single point work with this
     // handler! (error otherwise)
     if (meta->size != 0 &&
-        !is_in(meta->geometry_type, WK_POINT, WK_MULTIPOINT, WK_GEOMETRYCOLLECTION))
-      throw std::runtime_error("Can't convert geometry with type "s +
-                               std::to_string(meta->geometry_type) + " to h3 cell"s);
-
-    if (meta->size == 0) result_.push_back(h3_null);
+        !is_in(meta->geometry_type, WK_POINT, WK_MULTIPOINT, WK_GEOMETRYCOLLECTION)) {
+      auto msg = "[" + std::to_string(feat_id_ + 1) + "] Can't convert geometry with type " +
+                 std::to_string(meta->geometry_type) + " to cell";
+      throw std::runtime_error(msg);
+    }
 
     return Result::Continue;
   }
 
-  virtual Result coord(const wk_meta_t* meta, const double* coord) override {
-    // FIXME: limit to single coord
+  Result coord(const wk_meta_t* meta, const double* coord) override {
+    if (++coord_id_ != 0) {
+      auto msg =
+          "[" + std::to_string(feat_id_ + 1) + "] Feature contains more than one coordinate.";
+      throw std::runtime_error(msg);
+    }
+
     uint64_t cell;
     LatLng point = {degsToRads(coord[1]), degsToRads(coord[0])};
     if (auto err = latLngToCell(&point, res_, &cell); err != H3ErrorCodes::E_SUCCESS)
       throw std::runtime_error("H3 Error");
 
-    result_.push_back(cell == 0 ? h3_null : cell);
+    result_.push_back(cell);
     return Result::Continue;
   }
 
-  virtual SEXP vector_end(const wk_vector_meta_t* meta) override {
+  Result geometry_end(const wk_meta_t* meta) override {
+    // didn't write a coordinate?
+    if (coord_id_ == -1) result_.push_back(h3_null);
+    return Result::Continue;
+  }
+
+  SEXP vector_end(const wk_vector_meta_t* meta) override {
     result_.set_cls(vctrs_cls::h3_cell);
     return result_;
   }
 
 private:
   int res_;
+  uint64_t feat_id_ = -1;
+  uint32_t coord_id_ = -1;
   vctr<uint64_t, ProtectType::ObjectPreserve> result_;
 };
 
